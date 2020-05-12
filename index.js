43d871c20f8e8913f6eb0ca79244169b1b82d048
owenabbott <ocabbott@gmail.com>
2020-05-12 08:49:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
 // Your code goes here
-// document.addEventListener("DOMContentLoaded", function(){
-//     document.getElementById("text").innerHTML="This is really cool!")
-// }
 
-//curious why the above doesn't the above work. still learning. Comparing to solution file isn't very illuminating for this one:
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("text").innerHTML="This is really cool!"
+} )
 
-
-document.addEventListener( "DOMContentLoaded", function () {
-    updateDOM()
-  } );
-
-  function updateDOM() {
-    document.getElementById( "text" )
-      .innerHTML = "This is really cool!";
-  }
